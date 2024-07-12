77cfb852ec60189ddc8d0d1bd72c731db91ac08e
zehan12 <zehan9211@gmail.com>
2024-07-12 14:36:13 +0530

    feat: update log

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const app: Express = express();
 /* Define a route for the root path ("/")
  using the HTTP GET method */
 app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+  res.send("server is running");
 });
 
 
