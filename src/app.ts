90b16f2b452f172819365699077afd582599f49b
zehan12 <zehan9211@gmail.com>
2024-07-15 02:21:30 +0530

    feat: :construction: add middlewares like body parser and cookie parser

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,28 @@
 import express, { Express, Request, Response } from "express";
+import cookieParser from "cookie-parser";
 
 /*
  * Create an Express application and get the
  */
 const app: Express = express();
 
+// Middlewares ==>
+
+/*
+ * Set Body parser
+ * express().json() To parse JSON data in the req.body
+ * express().urlencoded() To parse Form data in the req.body
+ */
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// To set and get cookies
+app.use(cookieParser());
+
 /* Define a route for the root path ("/")
  using the HTTP GET method */
 app.get("/", (req: Request, res: Response) => {
-  res.send("server is running");
+    res.send("server is running");
 });
-
 
 export default app;
