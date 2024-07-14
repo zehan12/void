15b7591d37e717b61eff398a96ecf6a4a428eec9
zehan12 <zehan9211@gmail.com>
2024-07-15 01:44:11 +0530

    feat: :recycle: refactor code

--- src/server.ts
+++ src/server.ts
@@ -16,6 +16,51 @@ const PORT = process.env.PORT || 3000;
 
 /* Start the Express app and listen
  for incoming requests on the specified port */
-app.listen(PORT, () => {
-    console.log(`[server]: Server is running on port:${PORT}`);
+export const startServer = async () => {
+    const server = app.listen(PORT, () => {
+        console.log(`
+            ################################################
+            🚀 Server listening on port: ${PORT} 🚀
+            ################################################
+        `);
+    });
+
+    // Make sure we are running node 18+
+    const [major, minor] = process.versions.node.split(".").map(parseFloat);
+    if (major < 16 || (major === 16 && minor <= 0)) {
+        console.log(
+            "Please go to nodejs.org and download version 18 or greater. 👌\n "
+        );
+        process.exit();
+    }
+
+    const exitHandler = () => {
+        if (server) {
+            server.close(() => {
+                console.log("Server closed");
+                process.exit(1);
+            });
+        } else {
+            process.exit(1);
+        }
+    };
+
+    const unexpectedErrorHandler = (error: unknown) => {
+        console.log(error);
+        exitHandler();
+    };
+
+    process.on("uncaughtException", unexpectedErrorHandler);
+    process.on("unhandledRejection", unexpectedErrorHandler);
+
+    process.on("SIGTERM", () => {
+        console.log("SIGTERM received");
+        if (server) {
+            server.close();
+        }
+    });
+};
+
+startServer().then(() => {
+    console.log("server is up 🔥");
 });
